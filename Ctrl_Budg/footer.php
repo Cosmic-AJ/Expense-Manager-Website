868ea28c395d0e4205b9d66a8bb3db645d4909e4
Ayush Jain <ayushjain051001@gmail.com>
2021-01-09 23:44:21 +0530

    Add files via upload

---
+++ Ctrl_Budg/footer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!-- footer of the website -->
+<html>
+    <head>
+        <title>
+            footer
+        </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <!-- linking -->
+        <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css" >
+        <script src="jquery-3.4.1.min.js">
+        </script>
+        <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js">
+        </script>
+    </head>
+    <body>
+        <div class="footer container-fluid">
+            <!-- Content -->
+            Copyright &copy; Control budget. All Rights Reserved | Contact Us: +91-844844853
+        </div>
+    </body>
+</html>
